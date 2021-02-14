886e35061c3781fcaf38e4ca6d3058d3a7a922a3
William Dewailly <70532841+William-dew@users.noreply.github.com>
2021-02-14 14:16:37 +0100

    Update wp-config.php

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'tuto-chapellerie' );
+define( 'DB_NAME', 'chatelier' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'chatelier' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', 'chatelier' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
