cc9cf01fd8b7ad8708b8d9b1acdc3933370eb2e1
Etudiant O'clock <etudiant@teleporteur.oclock>
2021-02-16 14:17:18 +0100

    maj des 3k

---
+++ wordpress/content/plugins/chatelier/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '7e991e6c7b5d695d064a6129defe74f075ec99c7',
+    'name' => 'oclock/chatelier',
+  ),
+  'versions' => 
+  array (
+    'oclock/chatelier' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7e991e6c7b5d695d064a6129defe74f075ec99c7',
+    ),
+  ),
+);
