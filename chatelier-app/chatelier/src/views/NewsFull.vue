7b30e1e32a41cf8b3468da51303c3075d4f6e4f3
William-dew <william.dewailly@gmail.com>
2021-02-18 14:13:38 +0100

    update Post.vue

--- chatelier-app/chatelier/src/views/NewsFull.vue
+++ chatelier-app/chatelier/src/views/NewsFull.vue
@@ -1,15 +1,32 @@
 <template>
-<div>
-<h1>article</h1>
-
-</div>
+  <div class="p-10">
+    <Post :title="title" :content="content" :picture="picture" />
+  </div>
 </template>
-
 <script>
+import Post from "../components/Post";
+import PostService from "../services/PostService";
+
 export default {
+  components: {
+    Post,
+  },
+
   name: "NewsFull",
+
+  data: function () {
+    return { title: "", content: "", picture: "" };
+  },
+
+  beforeMount: function () {
+    PostService.getPost(this.$route.params.id).then((response) => {
+      console.log(response.data);
+      this.title = response.data.title.rendered;
+      this.content = response.data.content.rendered;
+      this.picture = response.data.featured_media_url;
+    });
+  },
 };
 </script>
-
 <style>
 </style>
