bd3672ada1bd52c74ccbe3621f33180e79d03959
William-dew <william.dewailly@gmail.com>
2021-02-22 15:27:01 +0100

    update homepage

--- chatelier-app/chatelier/src/views/Home.vue
+++ chatelier-app/chatelier/src/views/Home.vue
@@ -1,30 +1,24 @@
 <template>
-  <div>
-    <div class="">
-      <div class="absolute mt-4 ml-4 object-cover object-left">
-        <img
-          class="bg-center w-full h-96 object-cover "
-          src="../assets/images/moules.jpg"
-          alt
-        />
+  <div class>
+    <div class>
+      <div class="slide-left absolute mt-4 ml-4 object-cover object-left">
+        <img class="bg-center w-full object-cover photo" src="../assets/images/moules.jpg" alt />
       </div>
-      <div
-        class="w-2/4 z-10 h-full m-4 py-44 bg-white opacity-75 object-cover flex content-center justify-center"
-      >
-        <h1>
-          L'art de la chapellerie L'art de la chapellerie
-          <br />zezdzdzdzdzd
-          <br />zezdzdzdzdzd
-        </h1>
-        <div class="">
-          <button
-            class="border-2 border-green-400 text-green-400 p-2 px-2 h-12 uppercase hover:shadow hover:bg-green-400 hover:border-green-300 hover:text-white"
-          >Voir Trailer</button>
+      <div class="slide-right backg w-2/4 z-10 m-4 py-44 bg-white opacity-75 object-cover background">
+        <div class="flex justify-center">
+          <div class="">
+            <h1 class="text-5xl font-medium">L'art de la chapellerie</h1>
+            <h2 class="mt-2">Créez vos chapeaux</h2>
+          </div>
+          <div class="absolute mr-4">
+            <button
+              class="z-50 mt-44 ml-4 border-2 border-green-400 text-green-400 p-2 px-2 h-12 uppercase hover:shadow hover:bg-green-400 hover:border-green-300 hover:text-white"
+            >Voir Trailer</button>
+          </div>
         </div>
       </div>
     </div>
-
-    <Slider />
+    <Slider class="mt-40" />
   </div>
 </template>
 <script>
@@ -37,8 +31,57 @@ export default {
 <style>
 button {
   font-family: "rosario";
+  object-position: center;
 }
+.photo {
+  height: 590px;
+}
+.background {
+  height: 608px;
+}
+h1 {
+  font-family: "Calligraffitti", cursive;
+}
+h2 {
+  font-family: "rosario";
+  text-align: center;
+}
+.slide-left {
+ overflow: hidden; 
+}
+.slide-left img {
+  animation: 3s slide-right;
+  animation-delay: 0s;
+}
+.slide-right {
+   overflow: hidden;
+}
+.slide-right h1 {
+   animation: 3s slide-right;
+   animation-delay: 0s;
+}
+.slide-right h2 {
+   animation: 2.5s slide-right;
+   animation-delay: 0s;
+}
+.slide-right .backg {
+   animation: 2s slide-right;
+   animation-delay: 0s;
+}
+@keyframes slide-right {
+    from {
+       margin-left: -1000px;
+    }
+    to {
+       margin-left: 0%;
+    }
+ }
+ @keyframes slide-left {
+    from {
+       margin-left: -1000px;
+    }
+    to {
+       margin-left: 0%;
+    }
+ }
 </style>
-
-
-
