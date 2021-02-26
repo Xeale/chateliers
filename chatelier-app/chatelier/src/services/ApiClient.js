1bf1663725af2fc74835d385014a1d129606e6dd
William-dew <william.dewailly@gmail.com>
2021-02-26 15:00:07 +0100

    add metas on page "ressources"

--- chatelier-app/chatelier/src/services/ApiClient.js
+++ chatelier-app/chatelier/src/services/ApiClient.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 export const apiClient = axios.create({
-   // baseURL: "http://52.200.116.71/projet-tuto-chapellerie/wordpress/"
-    baseURL: "http://localhost/projet-tuto-chapellerie/wordpress/"
+   baseURL: "http://52.200.116.71/projet-tuto-chapellerie/wordpress/"
+   // baseURL: "http://localhost/projet-tuto-chapellerie/wordpress/"
 });
 
 export const baseUrlSuffix = "wp-json/wp/v2"
