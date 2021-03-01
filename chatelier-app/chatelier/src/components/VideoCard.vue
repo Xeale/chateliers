1dc46b8df8c660db147901012498b12fc27b564d
Xeale <70758685+Xeale@users.noreply.github.com>
2021-03-01 09:38:02 +0100

    Update VideoCard.vue

--- chatelier-app/chatelier/src/components/VideoCard.vue
+++ chatelier-app/chatelier/src/components/VideoCard.vue
@@ -2,13 +2,13 @@
   <router-link :to="{ name: 'courseFull', params: { id: courseID } }">
     <div class="m-4">
       <div
-        class="rounded overflow-hidden shadow-lg transform hover:scale-110 motion-reduce:transform-none transition duration-300 ease-in-out ..."
+        class="bg-white overflow-hidden shadow-lg transform hover:scale-110 motion-reduce:transform-none transition duration-300 ease-in-out ..."
       >
         <div class="px-6 py-4">
           <div class="font-bold text-xl mb-2">{{ title }}</div>
           <div class="wrapper-video">
             <video-embed :src="video"></video-embed>
-            <div v-html="content"></div>
+            <div class="pl-2" v-html="content"></div>
           </div>
         </div>
         <div class="px-6 pt-4 pb-2">
@@ -40,10 +40,10 @@ export default {
     content: String,
     courseID: Number,
     video: String,
-    levelList: [Array, Boolean],
-    materialList: [Array, Boolean],
-    styleList: [Array, Boolean],
-    toolList: [Array, Boolean],
+    levelList: Array,
+    materialList: Array,
+    styleList: Array,
+    toolList: Array,
     classSpan: String,
   },
 };
