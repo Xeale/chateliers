df9438a13a8bb2e4ea9123d6599bdd305376beab
William-dew <william.dewailly@gmail.com>
2021-03-01 16:25:35 +0100

    about page

---
+++ chatelier-app/chatelier/src/components/AboutComponents.vue
@@ -0,0 +1,14 @@
+<template>
+  <div v-html="content"></div>
+</template>
+
+<script>
+export default {
+  name: "AboutComponents",
+  props: { content: String },
+};
+</script>
+
+<style lang="scss">
+
+</style>
