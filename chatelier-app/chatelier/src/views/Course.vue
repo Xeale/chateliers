388287aec74d1547f6a15147de66a142f2da0091
Xeale <70758685+Xeale@users.noreply.github.com>
2021-03-01 09:35:46 +0100

    Update Course.vue

--- chatelier-app/chatelier/src/views/Course.vue
+++ chatelier-app/chatelier/src/views/Course.vue
@@ -1,6 +1,53 @@
 <template>
+<div>
+  <div id="SiteNav" class="site-nav" role="menu">
+          <ul class="list--nav">
+            
+              
+              
+                <li class="site-nav__element ">
+                  <a href="https://shop.hatacademy.com/collections/millinery-courses" class="site-nav__link">All Courses</a>
+                </li>
+              
+            
+              
+              
+                <li class="site-nav__element ">
+                  <a href="https://shop.hatacademy.com/pages/tutors" class="site-nav__link">Your Tutors</a>
+                </li>
+              
+            
+              
+              
+                <li class="site-nav__element ">
+                  <a href="/pages/millinery-supplies" class="site-nav__link">Millinery Supplies</a>
+                </li>
+              
+            
+              
+              
+                <li class="site-nav__element ">
+                  <a href="/pages/hat-blocks" class="site-nav__link">Hat Blocks</a>
+                </li>
+              
+            
+            
+          </ul>
+          <ul class="list--inline social-links">
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+          </ul>
+        </div>
   <div
-    class="bg-gray-200 p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-1"
+    class="bg-gray-100 p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-1"
   >
     <VideoCard
       v-for="course in courseList"
@@ -16,7 +63,9 @@
       v-bind:courseID="course.id"
       v-bind:classSpan="'inline-block bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2'"
     />
+    
   </div>
+</div>
 </template>
 <script>
 import CourseService from "../services/CourseService";
@@ -25,7 +74,7 @@ export default {
   components: {
     VideoCard,
   },
-  data() {
+  data: () => {
     return { courseList: [] };
   },
 
@@ -33,10 +82,12 @@ export default {
 
   created: function () {
     CourseService.getCoursesList()
+      //.then((data) => console.log(data.data[0].level_array[0].name))
       .then((data) => (this.courseList = data.data))
       .catch((error) => alert(error));
   },
 };
 </script>
 <style scoped>
+
 </style>
