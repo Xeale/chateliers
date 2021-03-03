40ae6099344425dcf0d8ed57b38654144b6ea673
William-dew <william.dewailly@gmail.com>
2021-03-03 14:21:05 +0100

    add button "back to top" on ressource page

--- chatelier-app/chatelier/src/views/Ressources.vue
+++ chatelier-app/chatelier/src/views/Ressources.vue
@@ -6,8 +6,10 @@
       <a href="#histoireMode">Histoire de la mode</a>
       <a href="#usage">Usage</a>
       <a href="#vocabulaire">vocabulaire</a>
+      <a href="#top" class="back-to-top">Back to top</a>
     </nav>
 
+    <a id="button">dgdsfgdsfgd</a>
     <Ressource
       v-bind:materiel="ressources.meta.materiel[0]"
       v-bind:fournisseur="ressources.meta.fournisseur[0]"
@@ -32,7 +34,12 @@ export default {
       .then((data) => (this.ressources = data.data))
       .catch((error) => alert(error));
   },
+  mounted: function() {
+    
+  }
+  
 };
+
 </script>
 <style lang="scss">
 .ressourcePage{
@@ -56,6 +63,63 @@ export default {
     margin-right: 2rem;
   }
 }
+#button {
+  display: inline-block;
+  color: red;
+  background-color: #FF9800;
+  width: 550px;
+  height: 50px;
+  text-align: center;
+  border-radius: 4px;
+  position: fixed;
+  bottom: 30px;
+  right: 30px;
+  transition: background-color .3s, 
+    opacity .5s, visibility .5s;
+  opacity: 0;
+  z-index: 1000;
+}
+#button::after {
+  content: "\f077";
+  font-family: FontAwesome;
+  font-weight: normal;
+  font-style: normal;
+  font-size: 2em;
+  line-height: 50px;
+  color: #fff;
+}
+#button:hover {
+  cursor: pointer;
+  background-color: #333;
+}
+#button:active {
+  background-color: #555;
+}
+#button.show {
+  opacity: 1;
+  visibility: visible;
+}
+    .back-to-top {
+        background-color:#333;
+        border:1px solid #222;
+        border-bottom:0;
+        bottom:10px;
+        color:#bbb;
+        // display:none;
+        // float:left;
+        font-size:11px;
+        padding:10px 10px 5px;
+        // position:fixed;
+        // right:530px;
+        text-decoration:none;
+        z-index:99999;
+        -webkit-border-radius:10px 10px 0 0;
+        -moz-border-radius:10px 10px 0 0;
+        border-radius:10px 10px 0 0;
+    }
+
+
+
 </style>
 
 
