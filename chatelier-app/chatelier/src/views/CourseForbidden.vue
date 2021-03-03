88f134b80cfe626c02a12019019deb883f798061
Etudiant O'clock <etudiant@teleporteur.oclock>
2021-03-03 14:23:13 +0100

    center login

--- chatelier-app/chatelier/src/views/CourseForbidden.vue
+++ chatelier-app/chatelier/src/views/CourseForbidden.vue
@@ -1,8 +1,8 @@
 <template>
-<div class="flex  flex-wrap justify-center ">
-  <h1>Veillez vous inscrire pour accéder aux cours</h1>
+<div class=" ">
+  <h1 class="flex flex-wrap justify-center">Veillez vous inscrire pour accéder aux cours</h1>
   <div class="">
-    <router-link class="  justify-center text-2xl p-2 text-green-300 border shadow-lg border-green-200 hover:border-gray-400 hover:text-gray-400 rounded pt-9." :to="{ name: 'login' }">Connexion</router-link>
+    <router-link class=" flex justify-center text-2xl px-3 text-green-300 border shadow-lg border-green-200 hover:border-gray-400 hover:text-gray-400 rounded pt-9." :to="{ name: 'login' }">Connexion</router-link>
   </div>
 </div>
 </template>
